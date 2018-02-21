a4e0ea6b2c6b9feb8add16ebcdc3905b7c49af7b
davidalves1 <alves.david@outlook.com>
2018-02-21 10:21:43 -0300

    Incluir data e hora do evento e corrigir o contador para considerar o fuso de São Paulo

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const deadline = new Date(Date.UTC(2018, 4, 12, 8)).getTime();
 
 const count = setInterval(function() {
-  const now = Date.now();
+  const now = Date.now() ;
   const diff = deadline - now;
 
   // https://www.w3schools.com/howto/howto_js_countdown.asp
   const days = Math.floor(diff / 1000 / 60 / 60 / 24);
-  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + 3; // Soma 3 para compensar o fuso
   hours = hours < 10 ? `0${hours}` : hours;
 
   let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
